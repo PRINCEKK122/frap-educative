843e5f69722f4ef3d2923dc69afc1c1ce23a6815
PRINCEKK122 <awuahkarikari12@gmail.com>
2022-12-07 23:04:41 +0000

    passing data from one component to another in react

--- front-end/react-examples/src/App.js
+++ front-end/react-examples/src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+/*
+  A bare bone react application 
+*/
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <p>FRAP applications are fun and powerful</p>
+//       </div>
+//     );
+//   }
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+/*
+  Components in React application
+*/
+// class Thing1 extends Component {
+//   render() {
+//     return (
+//       <div className="Thing1">
+//         <h2>Stuff here</h2>
+//       </div>
+//     );
+//   }
+// }
+
+// class Thing2 extends Component {
+//   render() {
+//     return (
+//       <div className="Thing2">
+//         <p>
+//           <strong>More stuff here</strong>
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <p>FRAP applications are fun and powerful</p>
+//         <Thing1 />
+//         <Thing2 />
+//         <Thing2 />
+//       </div>
+//     );
+//   }
+// }
+
+/*
+  Data in components example
+*/
+class Thing1 extends Component {
+  render() {
+    const { title, children } = this.props;
+    return (
+      <div className="Thing1">
+        <h2>{title}</h2>
+        {children}
+      </div>
+    );
+  }
+}
+
+class Thing2 extends Component {
+  render() {
+    const { gratitude } = this.props;
+    return (
+      <div className="Thing2">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I am grateful for <strong>{gratitude}</strong>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>FRAP applications are fun and powerul</p>
+        <Thing1 title="Grateful">Today...</Thing1>
+        <Thing2 gratitude="hot coffee" />
+        <Thing2 gratitude="bug-free code" />
+      </div>
+    );
+  }
 }
 
 export default App;
