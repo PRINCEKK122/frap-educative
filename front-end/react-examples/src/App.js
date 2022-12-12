f976727bda1f253a06c3ec9912c96d1c32568529
PRINCEKK122 <awuahkarikari12@gmail.com>
2022-12-12 08:31:07 +0000

    refactored code in App.js

--- front-end/react-examples/src/App.js
+++ front-end/react-examples/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 
 class Collatz extends Component {
   constructor(props) {
     super(props);
 
     let x = this.props.number;
-    let outputString = x.toString();
+    let outputArr = [x];
 
     while (typeof x === "number" && x !== 1) {
       if (x % 2 === 1) {
@@ -13,40 +14,77 @@ class Collatz extends Component {
       } else {
         x = x / 2;
       }
-      outputString += " " + x;
+      outputArr.push(x);
     }
-    this.outputString = outputString;
+    this.outputArr = outputArr;
   }
 
   render() {
     const { number } = this.props;
-    const outputString = this.outputString;
+    const outputArr = this.outputArr;
 
     return (
       <div className="Collatz">
         <h2>Collatz Sequence for {number}</h2>
-        <p>{outputString}</p>
+        <ul>
+          {outputArr.map((item, idx) => {
+            return <SpecialListItem key={idx}>{item}</SpecialListItem>;
+          })}
+        </ul>
       </div>
     );
   }
 }
 
+const GridDiv = styled.div`
+  display: grid;
+  grid-template-columns: 30% 70%;
+  grid-template-rows: auto;
+  grid-gap: 10px;
+  background-color: #eee;
+  color: black;
+  margin: 20px;
+`;
+const LeftColumn = styled.div`
+  grid-column: 1;
+  margin: 0.8em;
+`;
+const RightColumn = styled.div`
+  grid-column: 2;
+  margin: 0.8em;
+`;
+const EmphaticPar = styled.p`
+  font-style: oblique;
+`;
+const VeryEmphaticPar = styled(EmphaticPar)`
+  font-size: 150%;
+`;
+const SpecialListItem = styled.li`
+  font-weight: bold;
+  font-size: 90%;
+  padding: 2px;
+  list-style-type: ${(props) => props.listStyleType || "square"};
+`;
+
 class App extends Component {
   render() {
-    const number = 20;
+    const number = 4;
     const wikilink = "https://en.wikipedia.org/wiki/Collatz_conjecture";
     return (
-      <div className="App">
-        <h1>Collatz Conjecture</h1>
-        <p>
-          The <a href={wikilink}>Collatz Conjecture</a>&nbsp; states that for
-          any positive integer, <i>x</i>, repeatedly applying <i>3x + 1</i> if
-          odd and <i>x/2</i> if even will eventually lead to 1.
-        </p>
-        <p>No one knows if this is true.</p>
-
-        <Collatz number={number} />
-      </div>
+      <GridDiv>
+        <LeftColumn>
+          <h1>Collatz Conjecture</h1>
+          <p>
+            The <a href={wikilink}>Collatz Conjecture</a> states that for any
+            positive integer, <i>x</i>, repeatedly applying <i>3x + 1</i> if odd
+            and <i>x/2</i> if even will eventually led to 1.
+          </p>
+          <VeryEmphaticPar>No one knows if this is true.</VeryEmphaticPar>
+        </LeftColumn>
+        <RightColumn>
+          <Collatz number={number} />
+        </RightColumn>
+      </GridDiv>
     );
   }
 }
